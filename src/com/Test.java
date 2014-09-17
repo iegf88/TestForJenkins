5895ef3698c39addb51b1a54b5b44616d3f44b93
Ivan Gonzales <ivan.gonzalez@weather.com>
2014-09-16 22:06:16 -0400

    Adding sources

---
+++ src/com/Test.java
@@ -0,0 +1,8 @@
+package com;
+
+/**
+ * Created by ivan.gonzales on 9/16/14.
+ */
+public class Test {
+
+}
---
+++ src/com/TestTest.java
@@ -0,0 +1,11 @@
+package com;
+
+import junit.framework.TestCase;
+
+public class TestTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+}
